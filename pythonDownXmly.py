ec1c1aaf849ce89bfcbdcc8f9714761e403b3c02
hgz <huangguangzhong@glz8.com>
2018-01-12 22:35:45 +0800

    多进程优化

--- pythonDownXmly.py
+++ pythonDownXmly.py
@@ -1,6 +1,6 @@
 import  urllib.request
-import requests
-from multiprocessing import Queue,Pool
+import requests,queue
+from multiprocessing import Queue,Pool,Process
 import multiprocessing
 import  crontab,time,os
 from lxml import html
@@ -13,11 +13,9 @@ def cbk(a,b,c):
     per=100.0*a*b/c
     if per>100:
         per=100
-    print(f'{per}%')
-def get_url(siq_q):
-    req=requests.get('http://m.ximal'
-                     'aya.com/explore/more_album?page=1&per_page=12000&category_id=12&condition=rank&status=0').json()['html']
-    print(req)
+    print(f'{os.getpid() } --- {per}%')
+def get_url(q):
+    req=requests.get('http://m.ximalaya.com/explore/more_album?page=1&per_page=12000&category_id=12&condition=rank&status=0').json()['html']
     reqFrom=html.fromstring(req)
     hrefs=reqFrom.cssselect('a')
 
@@ -33,73 +31,36 @@ def get_url(siq_q):
                 f'http://m.ximalaya.com/album/more_tracks?url=%2Falbum%2Fmore_tracks&aid={tmpUrl}&page={page}').json()
             if infoByaid['next_page'] == 0:
                 stop = True
-
+                break
             sound_ids = requests.get(
                 f'http://m.ximalaya.com/album/more_tracks?url=%2Falbum%2Fmore_tracks&aid={tmpUrl}&page={page}').json()[
                 'sound_ids']
             for id in sound_ids:
-                siq_q.put(id)
-
-
-        siq_q.put(tmpUrl)
+                print(f'get{id}')
+                q.put(id)
 def get_sound_ids(sid_q):
-
     while True:
-        print(os.getppid())
-        tmpUrl=sid_q.get(True)
-        print(tmpUrl)
-        stop=False
-        page=0
-        while not stop:
-            page+=1
-            # print(tmpUrl)
-            infoByaid=requests.get(f'http://m.ximalaya.com/album/more_tracks?url=%2Falbum%2Fmore_tracks&aid={tmpUrl}&page={page}').json()
-            if infoByaid['next_page']==0:
-                stop=True
-
-            sound_ids=requests.get(f'http://m.ximalaya.com/album/more_tracks?url=%2Falbum%2Fmore_tracks&aid={tmpUrl}&page={page}').json()['sound_ids']
-            for id in sound_ids:
-                # print(id)
-                getInfo=requests.get(f'http://m.ximalaya.com/tracks/{id}.json').json()
-                if os.path.isfile(f'/data/down/mp3/{getInfo["title"]}.m4a'):
-                    continue
-                print(f'/data/down/mp3/{getInfo["title"]}.m4a{os.getppid()}')
-                # urllib.request.urlretrieve(f'{getInfo["play_path_64"]}',f'/data/down/mp3/{getInfo["title"]}.m4a',)
-
-
-            # print(sound_ids)
-        #     for k in range(len(sound_urls)):
-    #         sound_url=sound_urls[k].get('sound_url')
-    #         print(sound_url)
-    #         if(sound_url[0:4]=='http'):
-    #             urllib.request.urlretrieve(sound_url,f'/data/down/mp3/{titles[k]}.m4a',cbk)
-    #         else:
-    #             print('http://audio.pay.xmcdn.com/download/1.0.0/'+sound_url)
-    #             try :
-    #                 urllib.request.urlretrieve('http://audio.pay.xmcdn.com/download/1.0.0/'+sound_url, f'/data/down/mp3/{titles[k]}.m4a', cbk)
-    #             except urllib.error.HTTPError:
-    #                 break
-    #
+        id=sid_q.get(True)
+        print(id)
+        if not id:
+            continue
+        get_info=requests.get(f'http://m.ximalaya.com/tracks/{id}.json').json()
+        if os.path.isfile(f'/data/down/mp3/test/{get_info["title"]}.m4a'):
+            pass
+        else:
+            print(f'/data/down/mp3/test/test/{get_info["title"]}.m4a{os.getppid()}')
+            urllib.request.urlretrieve(f'{get_info["play_path_64"]}',f'/data/down/mp3/test/{get_info["title"]}.m4a',cbk)
 
 
 def main():
-
     manager = multiprocessing.Manager()
-
     q = manager.Queue()
-    p = Pool(processes=1)
-    pw1 = p.apply_async(get_url, args=(q,))
-    p1 = Pool(processes=4)
-    time.sleep(0.1)
-    for i in range(4):
-        time.sleep(i)
-        pr1 = p1.apply_async(get_sound_ids, args=(q,))
-    p1.close()
-    p1.join()
-
+    p = Pool(processes=4)
+    p.apply_async(get_url, args=(q,))
+    for i in range(3):
+        p.apply_async(get_sound_ids, args=(q,))
     p.close()
     p.join()
-    print(1)
 
 if __name__ == '__main__':
     main()
