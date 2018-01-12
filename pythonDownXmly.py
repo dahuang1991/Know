44d562501a056e48dcbfdc8bac51304eff55e3bc
hgz <huangguangzhong@glz8.com>
2018-01-12 23:11:16 +0800

    多进程优化

--- pythonDownXmly.py
+++ pythonDownXmly.py
@@ -1,27 +1,38 @@
-import  urllib.request
-import requests,queue
-from multiprocessing import Queue,Pool,Process
 import multiprocessing
-import  crontab,time,os
+import os
+import time
+import urllib.request
+from multiprocessing import Pool
+
+import requests
 from lxml import html
-def cbk(a,b,c):
+
+
+def cbk(a, b, c):
     '''''回调函数
     @a:已经下载的数据块
     @b:数据块的大小
     @c:远程文件的大小
     '''
-    per=100.0*a*b/c
-    if per>100:
-        per=100
+    per = 100.0 * a * b / c
+    if per > 100:
+        per = 100
     print(f'{os.getpid()}---------{per}%')
+
+"""
+    生产音频的id
+"""
+
 def get_url(q):
-    req=requests.get('http://m.ximalaya.com/explore/more_album?page=1&per_page=12000&category_id=12&condition=rank&status=0').json()['html']
-    reqFrom=html.fromstring(req)
-    hrefs=reqFrom.cssselect('a')
+    req = requests.get(
+        'http://m.ximalaya.com/explore/more_album?page=1&per_page=12000&category_id=12&condition=rank&status=0').json()[
+        'html']
+    reqFrom = html.fromstring(req)
+    hrefs = reqFrom.cssselect('a')
 
     print(len(hrefs))
     for i in hrefs:
-        tmpUrl=i.get('data-url').split('/')[3]
+        tmpUrl = i.get('data-url').split('/')[3]
         stop = False
         page = 0
         while not stop:
@@ -38,14 +49,18 @@ def get_url(q):
             for id in sound_ids:
                 # print(f'get{id}')
                 q.put(id)
+
+"""
+根据id下载音频
+"""
 def get_sound_ids(sid_q):
     while True:
-        id=sid_q.get(True)
+        id = sid_q.get(True)
         print(id)
         if not id:
             time.sleep(0.5)
             continue
-        get_info=requests.get(f'http://m.ximalaya.com/tracks/{id}.json').json()
+        get_info = requests.get(f'http://m.ximalaya.com/tracks/{id}.json').json()
         # print(get_info)
         if os.path.isfile(f'/data/down/mp3/test/{get_info["title"]}.m4a'):
             continue
@@ -53,8 +68,8 @@ def get_sound_ids(sid_q):
             if not get_info["play_path_64"]:
                 continue
             else:
-                urllib.request.urlretrieve(f'{get_info["play_path_64"]}',f'/data/down/mp3/test/{get_info["title"]}.m4a',cbk)
-
+                urllib.request.urlretrieve(f'{get_info["play_path_64"]}',
+                                           f'/data/down/mp3/test/{get_info["title"]}.m4a', cbk)
 
 
 def main():
@@ -67,8 +82,6 @@ def main():
     p.close()
     p.join()
 
+
 if __name__ == '__main__':
     main()
-
-
-
